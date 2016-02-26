0940ac8ef8458fcd8aa76c2a2b67301dfb9834dd
Gustavo Zambonin <gzmbnn@gmail.com>
2016-02-26 20:08:06 -0300

    iaa_calc.py: less naïve method of rounding the metrics

--- iaa_calc.py
+++ iaa_calc.py
@@ -8,12 +8,12 @@ from robobrowser import RoboBrowser
 
 
 def login(user, passwd):
-    browser = RoboBrowser(history=True, parser='html.parser')
-    browser.open('https://sistemas.ufsc.br/login')
+    browser = RoboBrowser(history=True, parser="html.parser")
+    browser.open("https://sistemas.ufsc.br/login")
 
-    form = browser.get_form(id='fm1')
-    form['username'].value = user
-    form['password'].value = passwd
+    form = browser.get_form(id="fm1")
+    form["username"].value = user
+    form["password"].value = passwd
     browser.submit_form(form)
 
     return browser
@@ -24,7 +24,7 @@ def get_student(browser):
     browser.open(url)
 
     if browser.url == url:
-        return browser.find(class_='rich-panel-header ').text
+        return browser.find(class_="rich-panel-header ").text
     raise SystemExit("Falha de autenticação!")
 
 
@@ -32,7 +32,7 @@ def get_history(browser):
     url = "https://cagr.sistemas.ufsc.br/modules/aluno/historicoEscolar/"
     browser.open(url)
 
-    hist = browser.find_all(class_='rich-table-cell ')
+    hist = browser.find_all(class_="rich-table-cell ")
     grades = [[int(hours.text), float(grade.text)]
               for hours, grade in zip(hist[2::7], hist[3::7])]
 
@@ -43,7 +43,7 @@ def get_current(browser):
     url = "https://cagr.sistemas.ufsc.br/modules/aluno/espelhoMatricula/"
     browser.open(url)
 
-    mirror = browser.find_all(class_='rich-table-cell ')
+    mirror = browser.find_all(class_="rich-table-cell ")
     current = [i.text for i in mirror if "id2" in str(i)]
     disciplines = [[name, int(hours)*18] for name, hours in
                    zip(current[3::10], current[5::10]) if int(hours)]
@@ -65,7 +65,7 @@ def ia_calc(grades):
     def sumproduct(lists):
         return sum(reduce(mul, data) for data in lists)
 
-    return float(str(sumproduct(grades) / sum(i[0] for i in grades))[:4])
+    return round(sumproduct(grades) / sum(i[0] for i in grades), 2)
 
 
 def iaa_poss(history, current):
@@ -82,8 +82,8 @@ history, current = get_history(browser), get_current(browser)
 print("Olá, {}! Seu IAA é {}.".format(student, ia_calc(history)))
 
 var = input("Deseja saber quanto seu IAA pode variar neste semestre? [s/N]: ")
-if var.lower() == 's':
-    print('Seu IAA pode variar de {} a {}.'
+if var.lower() == "s":
+    print("Seu IAA pode variar de {} a {}."
           .format(*iaa_poss(history, current)))
 
 for name, hours in current:
